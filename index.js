6fa1ab05d842dab6995dc0acd2a585a6cb6df545
bmandal806 <bmandal@pmsquare.com>
2025-07-18 11:40:43 +0545

    Second commit done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("Initial commit")
+console.log("this is the second commit but not kepp the changes")
+function demo(){
+    console.log("This is a demo function")
+}
+demo()
+const click= ()=>{
+console.log("This is the click function")
+
+}
