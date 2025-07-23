b382724af2039480fa4fddba8dd0247bfedc8709
bmandal806 <bmandal@pmsquare.com>
2025-07-23 11:03:22 +0545

    hello

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log("Initial commit")
 console.log("this is the second commit but not kepp the changes")
 function demo(){
+    console.log("You are inside the demo function body")
     console.log("This is a demo function")
 }
 demo()
@@ -9,5 +10,6 @@ const click= ()=>{
         console.log("This is the settimeout console")
     },2000)
 console.log("This is the click function")
+console.log("Missing the hello function")
 
 }
